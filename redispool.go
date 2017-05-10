48d4350bcee1c64c5872adb3e1d7169dde581e4a
rainfly <xiechc@gmail.com>
2017-05-10 17:51:21 +0800

    3#

--- redispool.go
+++ redispool.go
@@ -1,62 +1,33 @@
 package main
 
-import "menteslibres.net/gosexy/redis"
+import (
+	"fmt"
+	"github.com/mediocregopher/radix.v2/pool"
+)
 
-var host = "127.0.0.1"
-var port = uint(6379)
-var Clients redisPool
-
-type redisPool struct {
-	connections chan *redis.Client
-	connFn      func() (*redis.Client, error) // function to create new connection.
-}
-
-func (this *redisPool) Get() (*redis.Client, bool) {
-	var conn *redis.Client
-	select {
-	case conn = <-this.connections:
-	default:
-		conn, err := this.connFn()
-		if err != nil {
-			return nil, false
-		}
-
-		return conn, true
-	}
-
-	if err := this.testConn(conn); err != nil {
-		return this.Get() // if connection is bad, get the next one in line until base case is hit, then create new client
-	}
-
-	return conn, true
-}
-
-func (this *redisPool) Close(conn *redis.Client) {
-	select {
-	case this.connections <- conn:
-		return
-	default:
-		conn.Quit()
-	}
-}
-
-func (this *redisPool) testConn(conn *redis.Client) error {
-	if _, err := conn.Ping(); err != nil {
-		conn.Quit()
-		return err
-	}
-
-	return nil
-}
-func newcon() (*redis.Client, error) {
-	var client *redis.Client
-	client = redis.New()
-	err := client.Connect(host, port)
-	client.Command(nil, "SELECT", 1)
-	return client, err
-}
+var Clients *pool.Pool
 
 func InitRedis() {
-	Clients.connFn = newcon
-	Clients.connections = make(chan *redis.Client, 10)
+	var err error
+	Clients, err = pool.New("tcp", "localhost:6379", 10)
+	if err != nil {
+		// handle error
+	}
+
+}
+func main() {
+	InitRedis()
+	conn, err := Clients.Get()
+	if err != nil {
+		// handle error
+	}
+
+	r := conn.Cmd("GET", "globalid")
+	c, _ := r.Str()
+	fmt.Println(c)
+
+	r = conn.Cmd("GEORADIUS", "fuwa_c", 30000)
+	fmt.Println(r)
+
+	Clients.Put(conn)
 }
